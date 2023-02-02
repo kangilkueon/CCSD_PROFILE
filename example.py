c3b91ace56865d0cf5fdb3db830b180c0db19b54
kangilkueon <kangilkueon@gmail.com>
2023-02-02 20:34:19 +0900

    Some bug fix

--- example.py
+++ example.py
@@ -24,6 +24,7 @@ for line in lines:
             if len(words) > 3:
                 #print(words)
                 if words[len(words) - 1] == 'START':
+                    task_type = words[0]
                     worker_name = words[0] + '_' + words[1]
                     if worker_name not in worker_list:
                         worker_list.append(worker_name)
@@ -35,8 +36,9 @@ for line in lines:
                     if len(words) > 5:
                         task_name = task_name + '_' + words[3] + '_' + words[4]
 
-                    items.append({'worker_name':worker_name, 'task_name':task_name, 'start':time, 'end':0})
+                    items.append({'task_type':task_type, 'worker_name':worker_name, 'task_name':task_name, 'start':time, 'end':0})
                 if words[len(words) - 1] == 'END':
+                    task_type = words[0]
                     worker_name = words[0] + '_' + words[1]
                     task_name = words[2]
                     if len(words) > 5:
@@ -49,7 +51,7 @@ for line in lines:
                     time = line[0][1:idx]
 
                     for item in items[::-1]:
-                        if item['task_name'] == task_name and item['end'] == 0:
+                        if item['task_type'] == task_type and item['task_name'] == task_name and item['end'] == 0:
                             item['end'] = time
                             break
 f.close()
@@ -57,17 +59,17 @@ f.close()
 # Change font default
 gantt.define_font_attributes(fill='black', stroke='black', stroke_width=0, font_family="Verdana")
 
-print("len : ", len(items))
+#print("len : ", len(items))
 start_time = float(items[0]['start'])
 end_time = float(items[len(items) - 1]['end'])
-print(start_time)
-print(end_time)
+#print(start_time)
+#print(end_time)
 
 worker_object = []
 for worker in worker_list:
     p = gantt.Project(name=worker)
     worker_object.append({'name':worker, 'object':p})
-print(worker_list)
+#print(worker_list)
 p = gantt.Project(name='CCSD')
 # find Min term
 min = 0xFFFFFFFF
@@ -80,19 +82,16 @@ for item in items:
     if max < duration:
         max = duration
 
-print("d", min)
+#print("d", min)
 for item in items:
     start = ((float(item['start']) - start_time) * 1000000 / min)
     duration = ((float(item['end']) - float(item['start'])) * 1000000 / min)
-
     task = gantt.Task(name=item['task_name'], start=start, duration=duration)
 
     for worker in worker_object:
         if worker['name'] == item['worker_name']:
             worker['object'].add_task(task)
             break
-
-
 
 for po in worker_object:
     p.add_task(po['object'])
